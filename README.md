eedabe9712bd19fe6244fd9e6b7065a18f15d267
tharakanper <17931435+tharaka-perera@users.noreply.github.com>
2024-11-06 19:14:34 +1100

    Refactor code, update readme

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # pole_classifier
 End-to-end pipeline for pole classification using Google Street View images
+
+## Setup
+1. **Clone the repository:**
+   ```shell
+   git clone https://github.com/tharaka-perera/pole_classifier
+   cd <repository-directory>
+   ```
+
+2. **Create a virtual environment or conda environment:**
+```shell
+python -m venv venv
+source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+```
+
+3. **Install the required packages:**
+```shell
+pip install -r requirements.txt
+```
+
+4. **Set up environment variables:**
+Create a `.env` file in the root directory and add your API key:
+```shell
+API_KEY=your_api_key_here
+```
+
+## How to Run
+
+```shell
+python __main__.py
+```
+
+
+
+
+
+
---
+++ __main__.py
@@ -0,0 +1,49 @@
+import asyncio
+import fnmatch
+import os
+
+import numpy as np
+import pandas as pd
+import torch
+from geographiclib.geodesic import Geodesic
+from torchvision import ops
+from ultralytics import YOLO
+from ultralytics.engine.results import Boxes
+
+from utils.utils import download_images_from_df, get_heading_diff, wrap_around_heading, PanoramaViewer, \
+    sort_pano_locations, \
+    fetch_metadata_for_pano, process_pano_metadata, select_panos, process_images_with_yolo
+
+api_key = os.getenv("API_KEY")
+
+
+if __name__ == "__main__":
+    # # fetch meta-data from google using the location data and save to a csv file
+    # fetch_metadata_for_pano('data/resource_features_2.csv', 'data/cronulla_source_panoramas.csv')
+    #
+    # # filter too close or distance panos, too old panos and remove unnecessary columns from csv file
+    # df = pd.read_csv('data/cronulla_source_panoramas.csv')
+    # df = process_pano_metadata(df)
+    # df.to_csv('data/filtered_cronulla_panoramas.csv', index=False)
+    #
+    # # sort panoramas according to year and heading differences
+    # df = pd.read_csv('data/filtered_cronulla_panoramas.csv')
+    # df = sort_pano_locations(df)
+    # df.to_csv('data/sorted_cronulla_panoramas.csv', index=False)
+    #
+    # # select only 3 panoramas from each pole to download from the sorted list
+    # select_panos(3, 'data/sorted_cronulla_panoramas.csv', 'data/downloaded_cronulla_panoramas.csv')
+
+    with asyncio.Runner() as runner:
+        # download initial images asynchronously - make sure that the output directory is created
+        runner.run(download_images_from_df('data/downloaded_cronulla_panoramas.csv', 'data/source_cronulla_images'))
+
+        # download zoomed images and save calculated meta-data for images in a csv
+        model = YOLO('models/bestv10.pt')
+        processed_df = runner.run(
+            process_images_with_yolo(model, 'data/downloaded_cronulla_panoramas.csv', 'data/source_cronulla_images',
+                                     'data/zoomed_cronulla_images', 1000))
+        processed_df.to_csv('data/zoomed_panoramas_cronulla.csv', index=False)
+
+    # optional step to download images for pole material identification
+    # asyncio.run(download_material_images(model, '../data/zoomed_panoramas_brighten.csv', '../data/steel-poles/source'))
